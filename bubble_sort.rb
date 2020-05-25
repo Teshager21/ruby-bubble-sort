f591b1d3473df316c78838b2d6e5a7e3a1310c57
teshager21 <teshager8922@gmail.com>
2020-05-25 11:33:50 -0600

    add a bruteforce #bubble-sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,29 @@
-puts 'hello world!'
+arr = [9, 2, 5, 3, 10, 0]
+# 2,9,5,3,10,0
+# 2,5,9,3,10,0
+# 2,5,3,9,10,0
+# 2 5 3 10, 9 0
+# 2 5 3 10 0 9
+# 2 6 6 8 3 5
+#=====================#
+
+def swap(arr, first, second)
+  store = arr[first]
+  arr[first] = arr[second]
+  arr[second] = store
+  arr
+end
+
+#=====================#
+
+loop do
+  swap_counter = 0
+  (0...arr.length - 1).each do |i|
+    next unless (arr[i] <=> arr[i + 1]) == 1
+
+    p "#{arr} at #{swap_counter}"
+  end
+  puts "swap counter #{swap_counter}"
+  break swap_counter.zero?
+end
+p arr
