896f24a669aa42fd5f400e5bb0a886d0bde376d3
chirchir <chirchir7370@gmail.com>
2020-05-25 22:33:15 +0300

    clean up comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,3 @@
-arr = [9, 2, 5, 3, 10, 0]
-# 2,9,5,3,10,0
-# 2,5,9,3,10,0
-# 2,5,3,9,10,0
-# 2 5 3 10, 9 0
-# 2 5 3 10 0 9
-# 2 6 6 8 3 5
-#=====================#
 
 def swap(arr, first, second)
   store = arr[first]
@@ -14,22 +6,19 @@ def swap(arr, first, second)
   arr
 end
 
-#=====================#
 
-loop do
-  swap_counter = 0
-  (0...arr.length - 1).each do |i|
-    next unless (arr[i] <=> arr[i + 1]) == 1
-
-    swap(arr, i, i + 1)
-    swap_counter += 1
-    # else
-    #   next
-    # end
-
-    p "#{arr} at #{swap_counter}"
+def bubble_sort(arr)
+  loop do
+    swap_counter = 0
+    (0...arr.length - 1).each do |i|
+      next unless (arr[i] <=> arr[i + 1]) == 1
+  
+      swap(arr, i, i + 1)
+      swap_counter += 1
+    end
+    break if swap_counter.zero?
   end
-  puts "swap counter #{swap_counter}"
-  break if swap_counter.zero?
+  return arr
+  
 end
-p arr
+
