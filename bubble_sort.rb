9af4b3ec9c1c49f7520df5c3e7f28d4e15110aa4
chirchir <chirchir7370@gmail.com>
2020-05-25 22:34:59 +0300

    fix linter erros

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,3 @@
-
 def swap(arr, first, second)
   store = arr[first]
   arr[first] = arr[second]
@@ -6,19 +5,16 @@ def swap(arr, first, second)
   arr
 end
 
-
 def bubble_sort(arr)
   loop do
     swap_counter = 0
     (0...arr.length - 1).each do |i|
       next unless (arr[i] <=> arr[i + 1]) == 1
-  
+
       swap(arr, i, i + 1)
       swap_counter += 1
     end
     break if swap_counter.zero?
   end
-  return arr
-  
+  arr
 end
-
